f38633b161715ae0f49dafff3f8ac5e7fd7bd407
Luis Carvalho <la.carvalho@campus.fct.unl.pt>
2023-08-01 22:08:42 +0100

    Remove unneeded files.

--- vpy/lib/slice.py
+++ vpy/lib/slice.py
@@ -6,17 +6,21 @@ from vpy.lib.adapt import tr_class
 from vpy.lib.lib_types import VersionId
 from vpy.lib.transformers.module import ModuleTransformer
 from vpy.lib.utils import parse_class, parse_module
+from vpy.typechecker.checker import check_cls
 
 
-def rw_module(module: ModuleType, v: VersionId): # -> ModuleType
+def rw_module(module: ModuleType, v: VersionId):  # -> ModuleType
     mod_ast = ModuleTransformer(v).visit(parse_module(module))
     return [ast.unparse(ast.fix_missing_locations(mod_ast))]
 
 
 def eval_slice(module: ModuleType, cls: Type, v: VersionId) -> Type:
     cls_ast, _ = parse_class(module, cls)
-    sl = tr_class(module, cls_ast, v)
+    status, err = check_cls(module, cls_ast)
+    if not status:
+        raise Exception(err)
+    sl = ModuleTransformer(v).visit(cls_ast)
     s = ast.unparse(ast.fix_missing_locations(sl))
-    out = [None]
-    exec(s + f'\nout[0]={cls.__name__}_{v}')
+    out = [type]
+    exec(s + f"\nout[0]={cls.__name__}_{v}")
     return out[0]
