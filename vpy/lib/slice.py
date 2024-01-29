89357bb0a95365e940cc4ce16962452333fe124d
ecoop-108 <>
2024-01-29 15:44:43 +0000

    Rewrite module in runtime

--- vpy/lib/slice.py
+++ vpy/lib/slice.py
@@ -7,21 +7,25 @@ from vpy.lib import lookup
 
 from vpy.lib.lib_types import Environment, VersionId
 from vpy.lib.transformers.cls import ClassTransformer
+from vpy.lib.transformers.module import ModuleTransformer
 from vpy.lib.utils import parse_class, get_module_environment, parse_module
-from vpy.typechecker.checker import check_cls
+from vpy.typechecker.checker import check_cls, check_module
 
 
 def eval_slice(module: ModuleType, cls: Type, v: VersionId) -> Type:
-    mod_ast = parse_module(module)
-    mod_env = get_module_environment(mod_ast)
-
-    cls_ast, g = parse_class(module, cls)
-
-    status, err = check_cls(module, cls_ast, mod_env)
+    status, err = check_module(module)
     if not status:
         raise Exception(err)
-    sl = ClassTransformer(v=v, env=mod_env).visit(cls_ast)
-    s = ast.unparse(ast.fix_missing_locations(sl))
+    mod_ast = parse_module(module)
+    sl_mod = ModuleTransformer(v).visit(mod_ast)
+
+    sl_cls = [
+        c
+        for c in sl_mod.body
+        if isinstance(c, ast.ClassDef) and c.name == f"{cls.__name__}_{v}"
+    ][0]
+
+    s = ast.unparse(ast.fix_missing_locations(sl_cls))
     out = [type]
     exec(s + f"\nout[0]={cls.__name__}_{v}")
     return out[0]
