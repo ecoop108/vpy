c2715482196fe0fa41d83c21e5ea2eb021a40800
ecoop-108 <>
2024-03-08 12:57:29 +0000

    Examples

---
+++ examples/merge_conflict.py
@@ -0,0 +1,46 @@
+from vpy.decorators import at, get, run, version
+
+
+@version(name="init")
+@version(name="last", upgrades=["init"])
+@version(name="full", upgrades=["init"])
+@version(name="merge", replaces=["init", "full", "last"])
+class C:
+    """Version `init` introduces no methods. Then, branches `last` and `full` introduce separate definitions of method `m`.
+    Version `merge` joins versions `full` and `last` as a replacement for version `init`.
+    """
+
+    @at("full")
+    def b(self) -> str: ...
+
+    @at("last")
+    def b(self) -> str: ...
+
+    @at("merge")
+    def b(self) -> str:
+        """Version `merge` must introduce a definition of `m` to solve the conflict between the two branhces, otherwise the
+        program does not typecheck.
+        This definition is then available to clients in versions `init`, `full`, and `last`.
+        """
+        ...
+
+
+@version(name="init")
+@version(name="last", replaces=["init"])
+@version(name="full", replaces=["init"])
+@version(name="merge", replaces=["init", "full", "last"])
+class D:
+    """In this case, versions `last` and `full` replace version `init`. Each introduces a definition of method `m`.
+    This poses a conflict since definitions introduced in replacement versions should be available to previous clients.
+    As such, this program does not type check. We must remove either definition, or change the version graph
+    — introducing a merge definition does not resolve the conflict.
+    """
+
+    @at("full")
+    def b(self) -> str: ...
+
+    @at("last")
+    def b(self) -> str: ...
+
+    @at("merge")
+    def b(self) -> str: ...
--- examples/tchecker_call.py
+++ examples/tchecker_call.py
@@ -2,17 +2,32 @@ from vpy.decorators import at, get, run, version
 
 
 @version(name="init")
+@version(name="last", replaces=["init"])
 @version(name="full", replaces=["init"])
+@version(name="merge", replaces=["init", "full", "last"])
 class C:
-    @at("init")
-    def a(self) -> int:
-        return self.a()
+    # @at("init")
+    # def a(self) -> bool:
+    #     return True
 
     # @at("full")
     # def a(self) -> str:
-    #     x = 1
     #     return "1"
 
-    # @at("init")
-    # def b(self):
-    #     return self.a()
+    @at("full")
+    def b(self) -> str:
+        """This method is introduced in version full and available to all clients in version init."""
+        return "1 2".split()
+
+    @at("last")
+    def b(self) -> str:
+        """This replacement definition for `b` requires a lens from full -> last."""
+        return "1"
+
+    # @at("merge")
+    # def b(self) -> str:
+    #     return "1"
+
+    @at("init")
+    def m(self):
+        return self.b()
